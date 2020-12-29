15aa0ccbad9324309def4c0377c12bc728bbca70
naveen <g.naveenkumar0607@gmail.com>
2020-12-29 12:24:27 +0530

    finally

--- voting/views.py
+++ voting/views.py
@@ -4,8 +4,14 @@ from voting.models import candidates
 
 # Create your views here.
 def voting(request):
+    if request.method == 'POST':
+        val = request.POST['drop1']
+        val = positions.objects.filter(pname=val).first()
     context = {
         'positions': positions.objects.all(),
-        'candidates': candidates.objects.all()
+        'candidates': candidates.objects.all(),
+        'val': val
     }
     return render(request, 'voting/v_base.html', context)
+
+
